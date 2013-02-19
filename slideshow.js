d7ef034134caaf1baf25e953c9af9f81e3f784ae
Eddie Lee <eddieberklee@gmail.com>
2013-02-18 17:18:40 -0800

    woo all slideshows working, links for about me, no popup

--- slideshow.js
+++ slideshow.js
@@ -3,7 +3,13 @@
  * <.ss-left>
  * </.ss-left>
  * <.ss-main>
- *   <.page>
+ *   <.ss-page>
+ *     <.ss-row>
+ *       <a.circle></a.circle>
+ *       <a.circle></a.circle>
+ *       <a.circle></a.circle>
+ *       <a.circle></a.circle>
+ *     </.ss-row>
  *   </.page>
  * </.ss-main>
  * <.ss-right>
@@ -14,30 +20,37 @@
 $(function() {
   
   function sendToBack($this, $current) {
-    ss = $this.siblings('.ss-main').children('.ss-long-scroll');
+    ss = $this.siblings('.ss-main').children('.ss-page');
     $ss = $(ss);
-    console.log($ss.find($current).html());
-
-    $ss.append($current);
   }
   function sendToFront($this, $current) {
-    ss = $this.siblings('.ss-main').children('.ss-long-scroll');
+    ss = $this.siblings('.ss-main').children('.ss-page');
     $ss = $(ss);
-
-    $ss.find($current).remove();
-    $ss.prepend($current);
   }
+  var ss_width = 0;
+  var currenti = 0;
+  var comingupi;
 
   $(".ss-right").click(function() {
+    var ss_width = parseInt($(this).siblings('.ss-main').css('width')) + parseInt($(this).siblings('.ss-left').css('width')) + 'px';
     $this = $(this);
-    pages = $this.siblings('.ss-main').children('.ss-long-scroll').children('.ss-page')
+    pages = $this.siblings('.ss-main').children('.ss-page');
 
-    $current =  $(pages[0]);
-    $comingup = $(pages[1]);
-    var ss_width = $this.siblings('.ss-main').css('width');
+    $current =  $(pages[currenti]);
+    if ((currenti + 1) <= (pages.length - 1)) {
+      comingupi = currenti + 1;
+    } else {
+      comingupi = 0;
+    }
+    $comingup = $(pages[comingupi]);
+    // var ss_width = $this.siblings('.ss-main').css('width');
 
     $current.css('left', '0px');
-    $comingup.css('left', '820px');
+    $current.css('top', '0px');
+    $comingup.css('top', '0px');
+    $comingup.css('left', ss_width);
+
+    $comingup.css('z-index', parseInt($current.css('z-index'))+1);
 
     speed = 300;
     $current.stop().animate({
@@ -47,28 +60,40 @@ $(function() {
       left: '-=' + ss_width
     }, speed);
 
+    currenti = comingupi;
     sendToBack($this, $current);
   });
 
   $(".ss-left").click(function() {
+    var ss_width = parseInt($(this).siblings('.ss-main').css('width')) + parseInt($(this).siblings('.ss-right').css('width')) + 'px';
     $this = $(this);
-    pages = $this.siblings('.ss-main').children('.ss-long-scroll').children('.ss-page')
+    pages = $this.siblings('.ss-main').children('.ss-page');
 
-    $current =  $(pages[0]);
-    $comingup = $(pages[pages.length-1]);
-    var ss_width = $this.siblings('.ss-main').css('width');
+    $current =  $(pages[currenti]);
+    if ((currenti - 1) >= 0) {
+      comingupi = currenti - 1;
+    } else {
+      comingupi = pages.length-1;
+    }
+    $comingup = $(pages[comingupi]);
+    // var ss_width = $this.siblings('.ss-main').css('width');
 
     $current.css('left', '0px');
-    $comingup.css('left', '-'+ss_width);
+    $current.css('top', '0px');
+    $comingup.css('top', '0px');
+    $comingup.css('left', '-' + ss_width);
+
+    $comingup.css('z-index', parseInt($current.css('z-index'))+1);
 
     speed = 300;
     $current.stop().animate({
-      left: '+='+ss_width
+      left: '+=' + ss_width
     }, speed);
     $comingup.stop().animate({
-      left: '+='+ss_width
+      left: '+=' + ss_width
     }, speed);
 
-    sendToFront($this, $comingup);
+    currenti = comingupi;
+    sendToFront($this, $current);
   });
 });
