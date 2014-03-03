2a34a296c22bb545ec5447fb6e3bfc25bf815c4d
Ed Lee <eddieberklee@gmail.com>
2014-03-03 00:30:30 -0800

    deleting old lab files

--- labs/push.sh
+++
@@ -1,6 +0,0 @@
-git add .
-git ci -am "automated pushing"
-git remote add website ssh://ehdeelee_wooklee@ssh.phx.nearlyfreespeech.net/home/public/.git
-git push origin master
-git push website master
-
